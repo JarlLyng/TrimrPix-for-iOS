4df386e1df58fb5affb48e3a535fd6afc93371a3
Jarl Lyng <38857800+JarlLyng@users.noreply.github.com>
2026-03-24 22:02:59 +0100

    Initial Commit

---
+++ TrimrPix for iOS/TrimrPix_for_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrimrPix_for_iOSApp.swift
+//  TrimrPix for iOS
+//
+//  Created by Jarl Lyng on 24/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct TrimrPix_for_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
