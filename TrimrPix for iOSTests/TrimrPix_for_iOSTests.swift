6be641fcf744cb162bcf124b24c5229504781e19
Jarl Lyng <jarllyng@gmail.com>
2026-03-24 22:02:59 +0100

    Initial Commit

---
+++ TrimrPix for iOSTests/TrimrPix_for_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrimrPix_for_iOSTests.swift
+//  TrimrPix for iOSTests
+//
+//  Created by Jarl Lyng on 24/03/2026.
+//
+
+import Testing
+@testable import TrimrPix_for_iOS
+
+struct TrimrPix_for_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
